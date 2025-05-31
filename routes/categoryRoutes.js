57c1fe8fe2346346064d3290150a3f090d7b2295
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2025-05-31 10:55:50 +0600

    manage & delete categoryY

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -19,4 +19,29 @@ router.post('/add-category', async (req, res) => {
   }
 });
 
+
+// Route: GET all category
+router.get("/all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch categories", error });
+  }
+});
+
+
+// DELETE CATEGORY 
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting service", error });
+  }
+});
+
 module.exports = router;
